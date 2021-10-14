28fe426f398964b8ef8f3d4cf7c8c7845b3e2515
Benjamin Liden <benjamin.c.liden@gmail.com>
2021-10-14 18:35:56 -0400

    add format struct, gutter/bytes opts, make buf size dynamic, refactor get_line(s) to return formatted string, tests for util fns

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,12 @@ use std::path::PathBuf;
 use structopt::StructOpt;
 
 const ASCII_PERIOD: u8 = 46;
-const LINE_WIDTH: usize = 16;
+
+/**
+ * TODO:
+ * - [ ] gutter size of zero
+ * - [ ] fix spacing after ascii row when gutter is at end (cargo run ./src/main.rs -g 4 -n 8)
+ */
 
 #[derive(StructOpt, Debug)]
 #[structopt(name = "pseudo_hexdump", author = "Benjamin Liden")]
@@ -21,64 +26,114 @@ struct Opt {
     #[structopt(parse(from_os_str))]
     output: Option<PathBuf>,
 
-    // #[structopt(short, long, default_value = "16")]
-    // width: usize
+    // number of bytes wide to print
+    #[structopt(short, long, default_value = "16")]
+    num_bytes: usize,
+
+    // after how many bytes to print columns
+    #[structopt(short, long, default_value = "4")]
+    gutter_interval: usize,
+}
+
+struct Formatting {
+    bytes_to_read: usize,
+    gutter_per_n_bytes: usize,
+    ascii_line_width: usize,
+    hex_line_width: usize,
 }
 
 fn main() -> Result<(), Box<dyn error::Error>> {
     let opt = Opt::from_args();
-    // println!("{:#?}", opt);
+
+    let cfg = Formatting {
+        bytes_to_read: opt.num_bytes,
+        gutter_per_n_bytes: opt.gutter_interval,
+        ascii_line_width: (opt.num_bytes * 3)  // 2 chars per byte + 1 space
+            + (opt.num_bytes / opt.gutter_interval), // 1 char gutter per every n bytes
+        hex_line_width: opt.num_bytes,
+    };
 
     let mut file = File::open(&opt.input)?;
-    // let width = opt.width;
-
-    let mut buf: [u8; LINE_WIDTH] = [0; LINE_WIDTH];
-    let mut ln = LineCounter::new(LINE_WIDTH);
 
     let mut out: Box<dyn Write> = match opt.output {
-        Some(p) => Box::new(File::create(p).unwrap()),
+        Some(p) => Box::new(File::create(p)?),
         None => Box::new(io::stdout()),
     };
 
-    while file.read_exact(&mut buf).is_ok() {
-        write!(&mut out, "[{:#08x}] ", ln)?;
-        write_line_hex(&mut out, &buf)?;
-        write_line_ascii(&mut out, &buf)?;
-        writeln!(&mut out)?;
-        ln.increment();
+    let mut buf: Vec<u8> = vec![0; cfg.bytes_to_read];
+    let mut lc = LineCounter::new(cfg.bytes_to_read);
+
+    while let Ok(n_bytes) = file.read(&mut buf) {
+        // write ADDR
+        write!(out, "[{:#08x}]", lc)?;
+
+        // if no bytes, write \n and quit
+        if n_bytes == 0 {
+            writeln!(&mut out)?;
+            return Ok(());
+        }
+
+        let nbuf = &buf[..n_bytes];
+
+        // write HEX LINE
+        let line = get_line_hex(nbuf, &cfg)?;
+        let remainder = cfg.ascii_line_width.saturating_sub(line.len());
+        write!(out, "  {}{}", line, pad_spaces(remainder))?;
+
+        // write ASCII LINE
+        let line = get_line_ascii(nbuf, &cfg)?;
+        write!(out, "|{}|", line)?;
+
+        writeln!(out)?;
+        lc.increment();
     }
     Ok(())
 }
 
-fn write_line_hex(out: &mut Box<dyn Write>, buf: &[u8]) -> Result<(), Box<dyn error::Error>> {
+fn get_line_hex(buf: &[u8], cfg: &Formatting) -> Result<String, Box<dyn error::Error>> {
+    let mut s = Vec::<char>::with_capacity(cfg.ascii_line_width);
+
     for (index, byte) in buf.bytes().enumerate() {
         let byte = byte.unwrap_or(ASCII_PERIOD);
-        write!(out, "{:02x} ", byte)?;
+        s.extend(format!("{:02x} ", byte).chars());
 
-        // if we've printed 4 bytes, add a gutter
-        if (index + 1) % 4 == 0 {
-            write!(out, " ")?;
+        if is_gutter(index, cfg.gutter_per_n_bytes, cfg.bytes_to_read) {
+            s.push(' ');
         };
     }
-    Ok(())
+
+    Ok(s.into_iter().collect())
+}
+
+fn get_line_ascii(buf: &[u8], cfg: &Formatting) -> Result<String, Box<dyn error::Error>> {
+    let mut s = Vec::<char>::with_capacity(cfg.hex_line_width);
+
+    for byte in buf.bytes() {
+        match byte.unwrap_or(ASCII_PERIOD) as char {
+            c if is_crlf(c) => s.push('.'),
+            c if c.is_ascii_whitespace() => s.push(' '),
+            c if c.is_ascii_graphic() => s.push(c),
+            _ => s.push('.'),
+        };
+    }
+
+    Ok(s.into_iter().collect())
+}
+
+fn pad_spaces(n: usize) -> String {
+    let mut s = String::new();
+    for _ in 0..n {
+        s.push(' ');
+    }
+    s
+}
+
+fn is_gutter(index: usize, interval: usize, max: usize) -> bool {
+    (index + 1) % interval == 0 && (index + 1) != max
 }
 
 fn is_crlf(c: char) -> bool {
     c == 0x0a as char || c == 0x0d as char
-}
-
-fn write_line_ascii(out: &mut Box<dyn Write>, buf: &[u8]) -> Result<(), Box<dyn error::Error>> {
-    write!(out, "|")?;
-    for byte in buf.bytes() {
-        match byte.unwrap_or(ASCII_PERIOD) as char {
-            c if is_crlf(c) => write!(out, ".")?,
-            c if c.is_ascii_whitespace() => write!(out, " ")?,
-            c if c.is_ascii_graphic() => write!(out, "{}", c)?,
-            _ => write!(out, ".")?,
-        };
-    }
-    write!(out, "|")?;
-    Ok(())
 }
 
 #[cfg(test)]
@@ -86,12 +141,40 @@ mod tests {
     use super::*;
 
     #[test]
-    fn report_cr() {
-        assert_eq!(is_crlf('\r'), true)
+    fn reports_cr() {
+        assert_eq!(is_crlf('\r'), true);
+        assert_eq!(is_crlf(0x0a as char), true);
     }
 
     #[test]
     fn reports_lf() {
-        assert_eq!(is_crlf('\n'), true)
+        assert_eq!(is_crlf('\n'), true);
+        assert_eq!(is_crlf(0x0d as char), true);
+    }
+
+    #[test]
+    fn pad_spaces_works() {
+        assert_eq!(pad_spaces(0), String::from(""));
+        assert_eq!(pad_spaces(5), String::from("     "));
+        assert_eq!(pad_spaces(10), String::from("          "));
+    }
+
+    #[test]
+    fn is_gutter_works() {
+        let gutter_interval_in_bytes = 4;
+        let number_of_intervals = 4;
+        let max = gutter_interval_in_bytes * number_of_intervals;
+        let break_indices = vec![3, 7, 11];
+
+        for n in 0..max {
+            match n {
+                n if break_indices.contains(&n) => {
+                    assert_eq!(is_gutter(n, gutter_interval_in_bytes, max), true);
+                }
+                _ => {
+                    assert_eq!(is_gutter(n, gutter_interval_in_bytes, max), false);
+                }
+            };
+        }
     }
 }
