9f8b46ddb586ceb2c3ae308f15d1085aeac23a77
Benjamin Liden <benjamin.c.liden@gmail.com>
2021-10-13 23:58:53 -0400

    add structopt, output arg, fancy <dyn Write> trait

--- src/main.rs
+++ src/main.rs
@@ -2,47 +2,64 @@ mod line_counter;
 
 use line_counter::LineCounter;
 use std::char;
-use std::env;
 use std::error;
 use std::fs::File;
 use std::io::{self, Read, Write};
-use std::path::Path;
+use std::path::PathBuf;
+use structopt::StructOpt;
 
 const ASCII_PERIOD: u8 = 46;
 const LINE_WIDTH: usize = 16;
 
-fn main() -> Result<(), Box<dyn error::Error>> {
-    let args: Vec<String> = env::args().collect();
-    let path = args
-        .get(1)
-        .map(|p| Path::new(p))
-        .ok_or("Invalid path given")?;
+#[derive(StructOpt, Debug)]
+#[structopt(name = "pseudo_hexdump", author = "Benjamin Liden")]
+struct Opt {
+    #[structopt(parse(from_os_str))]
+    input: PathBuf,
 
-    let mut file = File::open(&path)?;
+    /// Output file, stdout if not present. Really should just use bash for this
+    #[structopt(parse(from_os_str))]
+    output: Option<PathBuf>,
+
+    // #[structopt(short, long, default_value = "16")]
+    // width: usize
+}
+
+fn main() -> Result<(), Box<dyn error::Error>> {
+    let opt = Opt::from_args();
+    // println!("{:#?}", opt);
+
+    let mut file = File::open(&opt.input)?;
+    // let width = opt.width;
+
     let mut buf: [u8; LINE_WIDTH] = [0; LINE_WIDTH];
     let mut ln = LineCounter::new(LINE_WIDTH);
 
-    let mut out= io::stdout();
-    // let mut out = File::create(Path::new("./out.txt"))?;
+    let mut out: Box<dyn Write> = match opt.output {
+        Some(p) => Box::new(File::create(p).unwrap()),
+        None => Box::new(io::stdout()),
+    };
 
     while let Ok(_) = file.read_exact(&mut buf) {
-        write!(out, "[{:#08x}] ", ln)?;
-        write_line_hex(Box::new(&mut out), &buf);
-        write_line_ascii(Box::new(&mut out), &buf);
-        write!(out, "\n")?;
+        write!(&mut out, "[{:#08x}] ", ln)?;
+        // write_line_hex(Box::new(&mut out), &buf);
+        // write_line_ascii(Box::new(&mut out), &buf);
+        write_line_hex(&mut out, &buf);
+        write_line_ascii(&mut out, &buf);
+        write!(&mut out, "\n")?;
         ln.increment();
     }
     Ok(())
 }
 
-fn write_line_hex(out: Box<&mut dyn Write>, buf: &[u8]) {
+fn write_line_hex(out: &mut Box<dyn Write>, buf: &[u8]) {
     for (index, byte) in buf.bytes().enumerate() {
         let byte = byte.unwrap_or(ASCII_PERIOD);
-        write!(*out, "{:02x} ", byte).ok();
+        write!(out, "{:02x} ", byte).ok();
 
         // if we've printed 4 bytes, add a gutter
         if (index + 1) % 4 == 0 {
-            write!(*out, " ").ok();
+            write!(out, " ").ok();
         };
     }
 }
@@ -51,17 +68,17 @@ fn is_crlf(c: char) -> bool {
     c == 0x0a as char || c == 0x0d as char
 }
 
-fn write_line_ascii(out: Box<&mut dyn Write>, buf: &[u8]) {
-    write!(*out, "|").ok();
+fn write_line_ascii(out: &mut Box<dyn Write>, buf: &[u8]) {
+    write!(out, "|").ok();
     for byte in buf.bytes() {
         match byte.unwrap_or(ASCII_PERIOD) as char {
-            c if is_crlf(c) => write!(*out, "{}", '.').ok(),
-            c if c.is_ascii_whitespace() => write!(*out, "{}", ' ').ok(),
-            c if c.is_ascii_graphic() => write!(*out, "{}", c).ok(),
-            _ => write!(*out, "{}", '.').ok(),
+            c if is_crlf(c) => write!(out, "{}", '.').ok(),
+            c if c.is_ascii_whitespace() => write!(out, "{}", ' ').ok(),
+            c if c.is_ascii_graphic() => write!(out, "{}", c).ok(),
+            _ => write!(out, "{}", '.').ok(),
         };
     }
-    write!(*out, "|").ok();
+    write!(out, "|").ok();
 }
 
 #[cfg(test)]
